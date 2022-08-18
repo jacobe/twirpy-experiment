c0949c50bd47beeda46cee2c3f2f1d05b1cd9f9d
Jacob Eisenberg <jacobe@github.com>
2022-08-18 15:43:46 +0200

    Add basic authorization middleware

--- server.py
+++ server.py
@@ -2,28 +2,39 @@ from twirp.asgi import TwirpASGIApp
 from rpc.test.service_twirp import TestServiceServer
 from twirp.exceptions import TwirpServerException
 import twirp.errors
+import twirp.context
 import rpc.test.service_pb2
 
 
-class TestService():
+class TestService:
 
-    def ListTests(self, context, request):
+    def ListTests(self, context: twirp.context.Context, request):
         pass
 
-    def GetTest(self, context, request):
+    def GetTest(self, context: twirp.context.Context, request):
         pass
 
-    def CreateTest(self, context, request):
+    def CreateTest(self, context: twirp.context.Context, request):
         return rpc.test.service_pb2.Test(id=1, name="test")
 
-    def UpdateTest(self, context, request):
+    def UpdateTest(self, context: twirp.context.Context, request):
         pass
 
-    def DeleteTest(self, context, request):
+    def DeleteTest(self, context: twirp.context.Context, request):
         raise TwirpServerException(code=twirp.errors.Errors.Unimplemented,
                                    message="Not implemented")
 
 
+async def authorize(context: twirp.context.Context, request: any, next):
+    headers = context.get('raw_headers')
+    auth = headers.get('authorization') if headers else None
+    if not auth:
+        raise TwirpServerException(code=twirp.errors.Errors.Unauthenticated,
+                                   message="Missing Authorization header")
+
+    return await next(context, request)
+
+
 service = TestServiceServer(service=TestService())
-app = TwirpASGIApp()
+app = TwirpASGIApp(authorize)
 app.add_service(service)
